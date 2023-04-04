d520a0b50326e09f11f5cc6f8a2c7bef59844777
pukar15 <np03cs4s220318@heraldcollege.edu.np>
2023-04-04 13:42:38 +0545

    test

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,4 +4,7 @@ router.get('/home', (req, res) => {
     res.render('dashboard/home.ejs',{user:req.user})
 })
 
+router.get('/post', (req, res) => {
+    res.render('dashboard/post.ejs',{user:req.user})
+})
 module.exports=router;
