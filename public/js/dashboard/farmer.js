f1793748fb501d9cd5a93bbebaf168b718d030a6
Rohan-Katwal <np03cs4s220276@heraldcollege.edu.np>
2023-04-05 14:14:58 +0545

    correction in farmer.ejs

--- public/js/dashboard/farmer.js
+++ public/js/dashboard/farmer.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     $('#example').DataTable();
 });
+
+const actionBtn = document.querySelectorAll('.action-menu i');
+
+for (var i = 0; i < actionBtn.length; i++) {
+    actionBtn[i].addEventListener('click', function(event){
+        this.nextElementSibling.classList.toggle('show');
+    });
+};
