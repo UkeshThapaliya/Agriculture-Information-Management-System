c8dea086e006738320f5742aab5d7470019fce09
Rohan-Katwal <np03cs4s220276@heraldcollege.edu.np>
2023-04-05 14:02:59 +0545

    farmer table backend

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,17 @@
 const router =require('express').Router();
+const {User}=require('../models/Model')
 
 router.get('/home', (req, res) => {
     res.render('dashboard/home.ejs',{user:req.user})
 })
 
 router.get('/farmer', (req, res) => {
-    res.render('dashboard/farmer.ejs',{user:req.user})
+    User.findAll({where:{role:"farmer"}}).then((farmer)=>{
+        console.log(farmer.length)
+        res.render('dashboard/farmer.ejs',{user:req.user,email:req.body.email,farmer})
+    }).catch((err)=>{
+        console.log(err)
+    })
 })
 
 module.exports=router;
