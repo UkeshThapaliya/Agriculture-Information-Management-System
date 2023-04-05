295b14184e796dece12494b360e09c90ed15df4f
Rohan-Katwal <np03cs4s220276@heraldcollege.edu.np>
2023-04-05 13:42:56 +0545

    farmer ejs routes and page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,4 +4,8 @@ router.get('/home', (req, res) => {
     res.render('dashboard/home.ejs',{user:req.user})
 })
 
+router.get('/farmer', (req, res) => {
+    res.render('dashboard/farmer.ejs',{user:req.user})
+})
+
 module.exports=router;
